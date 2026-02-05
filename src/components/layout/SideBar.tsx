052f3c2917b80852885e25b9631d4669356a5176
jjerry-o0o <alfndp25@gmail.com>
2026-02-05 21:32:58 +0900

    feat: 월별 > 레이아웃 작업중

--- src/components/layout/SideBar.tsx
+++ src/components/layout/SideBar.tsx
@@ -2,18 +2,20 @@ import { Link } from 'react-router-dom';
 
 const SideBar = () => {
   return (
-    <div className="w-20 h-full bg-sidebar flex flex-col">
-      <p>사이드바</p>
+    <div className="w-20 h-full bg-sidebar flex flex-col justify-between">
       <button className="border-2 w-14 h-14 rounded-4xl mx-auto">프로필</button>
-      <Link to="/" className="border-2 w-16 h-16 rounded-4xl mx-auto">
-        월별
-      </Link>
-      <Link to="/total" className="border-2 w-16 h-16 rounded-4xl mx-auto">
-        전재산
-      </Link>
-      <Link to="/finance" className="border-2 w-16 h-16 rounded-4xl mx-auto">
-        재테크
-      </Link>
+      <div className="flex flex-col flex-1 gap-3 border">
+        <Link to="/" className="border-2 w-16 h-16 rounded-4xl mx-auto">
+          월별
+        </Link>
+        <Link to="/total" className="border-2 w-16 h-16 rounded-4xl mx-auto">
+          전재산
+        </Link>
+        <Link to="/finance" className="border-2 w-16 h-16 rounded-4xl mx-auto">
+          재테크
+        </Link>
+      </div>
+      <button className="border-2 w-14 h-14 rounded-4xl mx-auto">설정</button>
     </div>
   );
 };
--- src/pages/MonthPage.tsx
+++ src/pages/MonthPage.tsx
@@ -1,7 +1,13 @@
 const MonthPage = () => {
   return (
-    <div>
-      <p className="text-black">Month</p>
+    <div className="flex w-full">
+      <div className="flex flex-1 border border-black w-full h-full">
+        <h3>2026년 2월</h3>
+        달력
+      </div>
+      <div className="flex border border-black w-100">
+        <p>일별 지출 목록</p>
+      </div>
     </div>
   );
 };
