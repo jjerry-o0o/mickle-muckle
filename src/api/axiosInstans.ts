5cff2bf2c8ee1f0d593561c0b6652386fb072cc9
jjerry-o0o <alfndp25@gmail.com>
2026-06-19 10:32:10 +0900

    fix: build 오류 수정 및 환경 파일 추가

--- src/api/axiosInstans.ts
+++ src/api/axiosInstans.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosApi = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   timeout: 3000,
   headers: { 'Content-Type': 'application/json' },
 });
